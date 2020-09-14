0b71d8ba9259862abaf91086e63dac2baa2b98df
Daniel Wei <danielwei816@gmail.com>
2020-09-13 21:59:44 -0400

    Accounted for duplicate file names

--- MediaGrouper.py
+++ MediaGrouper.py
@@ -1,18 +1,33 @@
 from PIL import Image
 import os, os.path
 
+
+"""
+path: the base path
+extensions: the list of extensions to be sorted
+folder: the name of the target folder
+"""
 def organize(path, extensions, folder):
     files = []
  
+    # finds all files in the base path that possess one of the extensions in extensions and appends it to the list of files
     for file in os.listdir(path): # lists all files + directories in specified directory
         ext = os.path.splitext(file)[1] # splits directory name into [root, ext]
         if ext.lower() in extensions:
             files.append(file)
 
     if not folder in os.listdir(path): os.mkdir(os.path.join(path, folder))
-
+    
     for file in files:
-        os.rename(os.path.join(path, file), os.path.join(os.path.join(path, folder), file))
+        suffixNum = 1
+        duplicateFile = file
+        while duplicateFile in os.listdir(os.path.join(path, folder)):
+        # looking for a duplicate file
+            duplicateFile = file + " (" + str(suffixNum) + ")"
+            suffixNum += 1
+            
+        os.rename(os.path.join(path, file), os.path.join(os.path.join(path, folder), duplicateFile))
+        
 
 base_path = "/Users/daniel/"
 folders = ["Desktop", "Downloads"]
